bea0721dc797fa6599e1230d1b5484e22117ea58
camlex-debug <20200003@colegioscj.edu.gt>
2025-05-14 15:26:29 -0600

    arreglo 2

--- Conversiones.py
+++ Conversiones.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 from tkinter import messagebox
 
-def conversion_de longitud(opcion, entrada, resultado):
+def conversion_de_longitud(opcion, entrada, resultado):
     try:
         valor = float(entrada.get())
         if opcion == 'm_km':
-            res = valor / 1000
-            resultado.config(text=f"{valor} metros = {res:.3f} kilómetros")
+            resultante = valor / 1000
+            resultado.config(text=f"{valor} metros = {resultante:.3f} kilómetros")
         elif opcion == 'in_m':
-            res = valor * 0.0254
-            resultado.config(text=f"{valor} pulgadas = {res:.3f} metros")
+            resultante = valor * 0.0254
+            resultado.config(text=f"{valor} pulgadas = {resultante:.3f} metros")
     except ValueError:
         messagebox.showerror("Error", "Ingresa un número válido")
