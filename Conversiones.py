6cab67ce5fead9b67b776f5b9f19d25189f98ac4
camlex-debug <20200003@colegioscj.edu.gt>
2025-05-14 15:35:18 -0600

    arreglo 3

--- Conversiones.py
+++ Conversiones.py
@@ -12,3 +12,15 @@ def conversion_de_longitud(opcion, entrada, resultado):
             resultado.config(text=f"{valor} pulgadas = {resultante:.3f} metros")
     except ValueError:
         messagebox.showerror("Error", "Ingresa un número válido")
+
+def conversion_de_masa(opcion, entrada, resultado):
+    try:
+        valor = float(entrada.get())
+        if opcion == 'kg_g':
+            resultante = valor * 1000
+            resultado.config(text=f"{valor} kg = {resultante:.2f} gramos")
+        elif opcion == 'lb_kg':
+            res = valor * 0.453592
+            resultado.config(text=f"{valor} lb = {resultante:.3f} kg")
+    except ValueError:
+        messagebox.showerror("Error", "Ingresa un número válido")
